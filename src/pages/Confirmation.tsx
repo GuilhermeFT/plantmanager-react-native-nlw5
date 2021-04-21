72a59ecc7dabee053994366b2a5b7e502f532101
Guilherme F. Trindade <38335563+GuilhermeFT@users.noreply.github.com>
2021-04-21 17:37:58 -0300

    Flatlists, fetch api, paginations and filter list

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/core";
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { Button } from "../components/Button";
@@ -5,6 +6,12 @@ import colors from "../styles/colors";
 import fonts from "../styles/fonts";
 
 export const Confirmation = () => {
+  const navigation = useNavigation();
+
+  function handleMoveOn() {
+    navigation.navigate("PlantSelect");
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
@@ -15,7 +22,7 @@ export const Confirmation = () => {
           Agora vamos começas a cuidar das saus plantinhas com muito cuidado.
         </Text>
         <View style={styles.footer}>
-          <Button title="Começar" />
+          <Button title="Começar" onPress={handleMoveOn} />
         </View>
       </View>
     </SafeAreaView>
